c03d105630b3a85cbfdc8341b3cf2b41c41c0788
TheZaran <138419233+TheZaran@users.noreply.github.com>
2025-02-16 22:26:01 +0500

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,100 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import React, { useEffect, useState } from 'react';
+import UserCard from "./components/userCard";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+const usersLimit = 15;
+//начало
+const Home = () => {
+    const [users, setUsers] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [search, setSearch] = useState('');
+    const [timeoutId, setTimeoutId] = useState(null);
+    const [totalPages, setTotalPages] = useState(0);
+
+//api inf
+    useEffect(() => {
+        const userFetch = async () => {
+            try {
+                const response = await fetch(`https://dummyjson.com/users/search?q=${search}&limit=${usersLimit}&skip=${(currentPage - 1) * usersLimit}`);
+
+                if (!response.ok) {
+                    throw new Error(`Ошибка HTTP: ${response.status}`);
+                }
+
+                const data = await response.json();
+                setUsers(data.users);
+                setTotalPages(Math.ceil(data.total / usersLimit));
+            } catch (error) {
+                console.error("Ошибка при загрузке пользователей:", error);
+            }
+        };
+        userFetch();
+    }, [currentPage, search]);
+
+//поиск
+    const handleSearch = (event) => {
+        const value = event.target.value;
+        setSearch(value);
+
+
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+
+
+        const id = setTimeout(() => {
+            setCurrentPage(1);
+        }, 1000);
+
+        setTimeoutId(id);
+    };
+
+//страницы
+    const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
+
+//хтмл
+    return (
+        <div>
+            <center>
+                <input
+                    type="text"
+                    placeholder="search"
+                    className="px-40 py-2 border border-solid border-gray-400 rounded-md mt-8"
+                    value={search}
+                    onChange={handleSearch}
+                />
+                <button onClick={()=>{
+                    setSearch('');
+                    setCurrentPage(1);
+                    if(timeoutId) clearTimeout(timeoutId);
+                }}
+                className='ml-4 px-4 py-2 bg-gray-300 rounded-md hover:bg-gray-400'
+                >
+                    Clear
+                </button>
+            </center>
+            <div className="user-list flex justify-center flex-wrap ">
+                {users.map(user => (
+                    <div key={user.id} className="hover:scale-105 transition-transform duration-300">
+                        <UserCard user={user} />
+                    </div>
+                ))}
+            </div>
+
+            <div className="pagination flex justify-center">
+                {pages.map(page => (
+                    <button
+                        key={page}
+                        onClick={() => setCurrentPage(page)}
+                        className={`mx-1 px-3 py-1 border rounded-lg ${currentPage === page ? 'bg-black text-white' : 'bg-white text-gray-800 hover:bg-gray-100'}`}
+                    >
+                        {page}
+                    </button>
+                ))}
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+    );
+};
+
+export default Home;
